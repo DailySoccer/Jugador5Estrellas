ce4e07ae9473dc1963dd20dd1ced182d1c0168fc
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-10-13 16:09:34 +0200

    Light-map fixing (erasing, better said...)

---
+++ Assets/Scripts/DontDestroyOnLoadExceptRepeated.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyOnLoadExceptRepeated : MonoBehaviour {
+
+	private static int counter = 0;
+	// Use this for initialization
+	void Awake () {
+		if (counter != 0)
+		{
+			GameObject aux = GameObject.FindGameObjectWithTag(name);
+			if (aux != null)
+			{
+				DestroyImmediate(gameObject);
+			}
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
